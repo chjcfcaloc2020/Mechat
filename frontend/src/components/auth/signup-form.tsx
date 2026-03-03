fd66c2e464ec67ec849f5a75312454a1154be5f2
chjcfcaloc2020 <nguyentruongchi83@gmail.com>
2026-03-03 10:24:09 +0700

    add signin and signup ui

---
+++ frontend/src/components/auth/signup-form.tsx
@@ -0,0 +1,122 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const signUpSchema = z.object({
+  firstname: z.string().min(1, "Tên không được để trống"),
+  lastname: z.string().min(1, "Họ không được để trống"),
+  username: z.string().min(3, "Tên đăng nhập phải có ít nhất 3 ký tự"),
+  email: z.email("Email không hợp lệ"),
+  password: z.string().min(6, "Mật khẩu phải có ít nhất 6 ký tự"),
+})
+
+type SignUpFormValues = z.infer<typeof signUpSchema>
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const {register, handleSubmit, formState: { errors, isSubmitting }} = useForm<SignUpFormValues>({
+    resolver: zodResolver(signUpSchema),
+  })
+
+  const onSubmit = async (data: SignUpFormValues) => {
+
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0 border-border">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center gap-2">
+                <a href="/" className="mx-auto block w-fit text-center">
+                  <img src="/logo.png" alt="Logo" width={60} height={60}/>
+                </a>
+                <h1 className="text-2xl font-bold">Tạo tài khoản Mechat</h1>
+                <p className="text-muted-foreground text-balance text-sm">
+                  Chào mừng bạn! Hãy đăng ký để bắt đầu.
+                </p>
+              </div>
+
+              <div className="grid grid-cols-2 gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="lastname" className="block text-sm">
+                    Họ
+                  </Label>
+                  <Input type="text" id="lastname" {...register("lastname")} />
+                  {errors.lastname && (<p className="text-sm text-destructive">{errors.lastname.message}</p>)}
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="firstname" className="block text-sm">
+                    Tên
+                  </Label>
+                  <Input type="text" id="firstname" {...register("firstname")} />
+                  {errors.firstname && (<p className="text-sm text-destructive">{errors.firstname.message}</p>)}
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="username" className="block text-sm">
+                    Tên đăng nhập
+                  </Label>
+                  <Input type="text" id="username" placeholder="Mechat" {...register("username")} />
+                  {errors.username && (<p className="text-sm text-destructive">{errors.username.message}</p>)}
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="email" className="block text-sm">
+                    Email
+                  </Label>
+                  <Input type="email" id="email" placeholder="test@gmail.com" {...register("email")} />
+                  {errors.email && (<p className="text-sm text-destructive">{errors.email.message}</p>)}
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="password" className="block text-sm">
+                    Mật khẩu
+                  </Label>
+                  <Input type="password" id="password" {...register("password")} />
+                  {errors.password && (<p className="text-sm text-destructive">{errors.password.message}</p>)}
+                </div>
+              </div>
+
+              <Button type="submit" className="w-full" disabled={isSubmitting}>
+                Tạo tài khoản
+              </Button>
+
+              <div className="text-center text-sm">
+                Đã có tài khoản? {" "}
+                <a href="/signin" className="underline underline-offset-4">
+                  Đăng nhập
+                </a>
+              </div>
+            </div>
+          </form>
+          <div className="relative hidden bg-muted md:block">
+            <img
+              src="/img_signup.png"
+              alt="Image"
+              className="absolute object-cover inset-0 h-full w-full"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-xs text-balance px-6 text-center *:[a]:hover:text-primary text-muted-foreground *:[a]:underline *:[a]:underline-offset-4">
+        Bằng cách nhấp vào tiếp tục, bạn đồng ý với <a href="#">Điều khoản dịch vụ</a>{" "}
+        và <a href="#">Chính sách bảo mật</a> của chúng tôi.
+      </div>
+    </div>
+  )
+}
